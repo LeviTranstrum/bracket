2575f6b4af8617bd6667f281eeed64027a9448ae
LOG2144062 <LOG2144062@maricopa.edu>
2024-12-12 19:31:18 -0700

    Syncing my files from C# player's guide to Levi's repo

---
+++ Class.cs
@@ -0,0 +1,53 @@
+﻿//Class.cs
+
+using System;
+using System.Collections.Generic;
+
+namespace BracketClasses
+{
+    public class Team
+    {
+        public string City { get; set; }
+        public string TeamName { get; set; }
+        public int Ranking { get; set; }
+
+        public Team(string city, string teamName, int ranking)
+        {
+            City = city;
+            TeamName = teamName;
+            Ranking = ranking;
+        }
+
+        public static void DisplayMatchTeams(List<Team> teams)
+        {
+            foreach (var team in teams)
+            {
+                Console.WriteLine($"The {team.City} {team.TeamName} have a ranking of {team.Ranking}.");
+            }
+        }
+        public static void TeamBattle(List<Team> teams)
+        {
+            Random random = new();
+            int Team1 = random.Next(teams.Count);
+            int Team2 = random.Next(teams.Count);
+
+
+            if (teams[Team1].Ranking > teams[Team2].Ranking)
+            {
+                Console.WriteLine($"The {teams[Team1].City} {teams[Team1].TeamName} have a ranking of {teams[Team1].Ranking}.");
+                Console.WriteLine($"The {teams[Team2].City} {teams[Team2].TeamName} have a ranking of {teams[Team2].Ranking}.");
+                Console.WriteLine($"The {teams[Team1].City} {teams[Team1].TeamName} are higher ranked.");
+            }
+
+            else
+            {
+                Console.WriteLine($"The {teams[Team2].City} {teams[Team2].TeamName} have a ranking of {teams[Team2].Ranking}.");
+                Console.WriteLine($"The {teams[Team1].City} {teams[Team1].TeamName} have a ranking of {teams[Team1].Ranking}.");
+                Console.WriteLine($"The {teams[Team2].City} {teams[Team2].TeamName} are higher ranked.");
+            }
+
+        }
+
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,51 @@
+﻿//Main.cs
+
+using BracketClasses;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+
+        var teams = new List<Team>
+      {
+        new Team("Arizona", "Diamondbacks", 3),
+        new Team("Oakland", "Athletics", 4),
+        new Team("Atlanta", "Braves", 2),
+        new Team("Baltimore", "Orioles", 2),
+        new Team("Boston", "Red Sox", 3),
+        new Team("Chicago", "Cubs", 2),
+        new Team("Chicago", "White Sox", 5),
+        new Team("Cincinnati", "Reds", 4),
+        new Team("Cleveland", "Guardians", 1),
+        new Team("Colorado", "Rockies", 5),
+        new Team("Detroit", "Tigers", 3),
+        new Team("Houston", "Astros", 1),
+        new Team("Kansas City", "Royals", 2),
+        new Team("Los Angeles", "Angels", 5),
+        new Team("Los Angeles", "Dodgers", 1),
+        new Team("Miami", "Marlins", 5),
+        new Team("Milwaukee", "Brewers", 1),
+        new Team("Minnesota", "Twins", 4),
+        new Team("New York", "Mets", 3),
+        new Team("New York", "Yankees", 1),
+        new Team("Philadelphia", "Phillies", 1),
+        new Team("Pittsburgh", "Pirates", 5),
+        new Team("San Diego", "Padres", 2),
+        new Team("San Francisco", "Giants", 4),
+        new Team("Seattle", "Mariners", 2),
+        new Team("St. Louis", "Cardinals", 3),
+        new Team("Tampa Bay", "Rays", 4),
+        new Team("Texas", "Rangers", 3),
+        new Team("Toronto", "Blue Jays", 5),
+        new Team("Washington", "Nationals", 4)
+       };
+
+        Team.DisplayMatchTeams(teams);
+
+
+
+    }
+}
